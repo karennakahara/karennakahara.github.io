d6f7f938087246ca460947b0191c76c5f48a458b
karennakahara <138583791+karennakahara@users.noreply.github.com>
2023-07-04 05:14:46 -0600

    Add files via upload

---
+++ Bit.js
@@ -0,0 +1,11 @@
+"use strict"
+function cambia(n,r) {
+    if (r=="si") {
+        document.getElementById(n).style.background= "#FFC0CB"
+    } else{
+        document.getElementById(n).style.background= ""
+    }
+}
+
+/*BITACORA*/
+
---
+++ proyectoK.js
@@ -0,0 +1,15 @@
+"use strict"
+function cambia(n,r) {
+    if (r=="si") {
+        document.getElementById(n).style.background= "#FFDAB9"
+    } else{
+        document.getElementById(n).style.background= ""
+    }
+}
+
+
+
+
+
+
+
